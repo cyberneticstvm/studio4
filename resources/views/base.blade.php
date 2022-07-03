22f9325ebe062256aa40ee41936cf796190592dc
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-07-03 20:43:30 +0530

    small updates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -103,6 +103,9 @@
                         </div>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link" href="/">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="#contact">Contact</a>
                     </li>
                 </ul>
