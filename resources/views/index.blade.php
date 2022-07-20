2a2515643c0e2fa2f481ae43730568a7f1dd99dd
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-07-20 17:28:17 +0530

    small updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -128,7 +128,7 @@
                 <div class="col-lg-7 valign">
                     <div class="exp-content wow fadeInUp" data-wow-delay=".3s">
                         <h6 class="sub-title">About Us</h6>
-                        <h2 class="mb-20 color-pink">Best Interior Designers <br> For You.</h2>
+                        <h2 class="mb-20">Best Interior Designers <br> For You.</h2>
                         <p class="text-justify">Studio1 Creators are recognized as a leading professional providing excellent service for all types of interior architecture and architectural design projects, well established in the United Arab Emirates, our services extend throughout the GCC region. We are in the business of designing spaces by understanding and providing solutions that consider cost, time and quality - ensuring every project is of the highest quality.</p>
                         <p class="text-justify">Studio1 Creators also provides full-service interior design company providing luxury interior design and home décor services. Our services include Interior design, Architecture, Landscape and Fit-outs for residential, commercial, hospital, food & beverage units.</p>
                         <div class="numbers mt-50">
